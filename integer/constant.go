7b8c1ec4d6d1c75debbdfc33b4c3d1848a1febda
ender <xuender@gmail.com>
2020-03-15 11:45:34 +0800

    constant doc

--- integer/constant.go
+++ integer/constant.go
@@ -1,11 +1,11 @@
 package integer
 
 // MaxInt 最大整数
-// 64 位等于 math.MaxUint64
-// 32 位等于 math.MaxUint32
+// 64 位等于 math.MaxInt64
+// 32 位等于 math.MaxInt32
 const MaxInt = int(^uint(0) >> 1)
 
 // MinInt 最小整数
-// 64 位等于 math.MinUint64
-// 32 位等于 math.MinUint32
+// 64 位等于 math.MinInt64
+// 32 位等于 math.MinInt32
 const MinInt = ^MaxInt
