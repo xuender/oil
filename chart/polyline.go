2e3a2847e66e7883e2eacdf64a5d29f5d59ee552
ender <xuender@gmail.com>
2020-03-21 12:01:39 +0800

    chart format

--- chart/polyline.go
+++ chart/polyline.go
@@ -8,7 +8,15 @@ import (
 )
 
 // 模板
-const _svg = `<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg version="1.1" width="%d" height="%d" xmlns="http://www.w3.org/2000/svg"><rect height="100%%" width="100%%" fill="#f3f3f3"/>%s%s%s%s</svg>`
+const _svg = `<?xml version="1.0" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg version="1.1" width="%d" height="%d" xmlns="http://www.w3.org/2000/svg">
+<rect height="100%%" width="100%%" fill="#f3f3f3"/>
+%s
+%s
+%s
+%s
+</svg>`
 
 // SVG预估尺寸
 const _size = 800
