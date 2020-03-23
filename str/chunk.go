2f81fc0836883e56e6dc1be9cb032d8fe365ee74
ender <xuender@gmail.com>
2020-03-23 19:14:33 +0800

    add Chunk

---
+++ str/chunk.go
@@ -0,0 +1,27 @@
+package str
+
+import (
+	"github.com/xuender/oil/integer"
+)
+
+// Chunk 分组
+func Chunk(str string, size int) []string {
+	length := len(str)
+	if length == 0 || size < 1 {
+		return []string{}
+	}
+	ret := make([]string, integer.Ceil(length, size))
+
+	index := 0
+	i := 0
+	for index < length {
+		end := index + size
+		if end > length {
+			end = length
+		}
+		ret[i] = str[index:end]
+		index = end
+		i++
+	}
+	return ret
+}
---
+++ str/chunk_test.go
@@ -0,0 +1,29 @@
+package str
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestChunk(t *testing.T) {
+	type args struct {
+		str  string
+		size int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{name: "123 | 2", args: args{str: "123", size: 1}, want: []string{"1", "2", "3"}},
+		{name: "12 | 2", args: args{str: "12", size: 2}, want: []string{"12"}},
+		{name: "456 | 2", args: args{str: "456", size: 2}, want: []string{"45", "6"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Chunk(tt.args.str, tt.args.size); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Chunk() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
