4251188540bba9e895cc7756b1ba2bd946ce9023
ender <xuender@gmail.com>
2020-09-08 10:28:57 +0800

    base64 url

--- basex/basex.go
+++ basex/basex.go
@@ -4,4 +4,4 @@ package basex
 var Base57 = NewEncoding("23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
 
 // Base64 is a radix 64 encoding/decoding scheme
-var Base64 = NewEncoding("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/")
+var Base64 = NewEncoding("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_")
