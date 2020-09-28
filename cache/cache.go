0a8b99682c426cf1dba87e801f5775bc03555317
ender <xuender@gmail.com>
2020-09-28 14:48:38 +0800

    feat(cache): 增加简单的内存缓存

---
+++ cache/cache.go
@@ -0,0 +1,159 @@
+// Package cache 简单的内存缓存.
+//
+// 如果需要复杂的功能可以使用 github.com/allegro/bigcache.
+package cache
+
+import (
+	"sync"
+	"time"
+)
+
+// Cache support LRU (Least Recently Used).
+type Cache struct {
+	lock   sync.RWMutex
+	data   map[interface{}]interface{}
+	access map[interface{}]time.Time
+
+	Expire   time.Duration
+	LRU      bool
+	Callback func(key, value interface{})
+}
+
+// Set value by key.
+func (c *Cache) Set(key, value interface{}) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.data[key] = value
+	c.access[key] = time.Now().Add(c.Expire)
+}
+
+// SetByTime value by key.
+func (c *Cache) SetByTime(key, value interface{}, expire time.Time) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.data[key] = value
+	c.access[key] = expire
+}
+
+// SetByDuration value by key.
+func (c *Cache) SetByDuration(key, value interface{}, expire time.Duration) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.data[key] = value
+	c.access[key] = time.Now().Add(expire)
+}
+
+// Get value by key.
+func (c *Cache) Get(key interface{}) (interface{}, bool) {
+	c.lock.RLock()
+	value, ok := c.data[key]
+	c.lock.RUnlock()
+	if c.LRU {
+		c.Reset(key)
+	}
+	return value, ok
+}
+
+// GetString by key.
+func (c *Cache) GetString(key interface{}) (string, bool) {
+	if value, ok := c.Get(key); ok {
+		return value.(string), ok
+	}
+	return "", false
+}
+
+// Reset expire by key.
+func (c *Cache) Reset(key interface{}) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	if a, ok := c.access[key]; ok {
+		e := time.Now().Add(c.Expire)
+		if e.After(a) {
+			c.access[key] = e
+		}
+	}
+}
+
+// Keys by map.
+func (c *Cache) Keys() []interface{} {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	keys := make([]interface{}, len(c.data))
+	i := 0
+	for k := range c.data {
+		keys[i] = k
+		i++
+	}
+	return keys
+
+}
+
+// Del key.
+func (c *Cache) Del(key interface{}) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	if value, ok := c.data[key]; ok {
+		c.Callback(key, value)
+	}
+	delete(c.data, key)
+	delete(c.access, key)
+}
+
+// Clean overdue.
+func (c *Cache) Clean(overdue time.Time) {
+	for _, key := range c.Overdue(overdue) {
+		c.Del(key)
+	}
+}
+
+// Overdue keys.
+func (c *Cache) Overdue(overdue time.Time) []interface{} {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	keys := []interface{}{}
+	for key, v := range c.access {
+		if overdue.After(v) {
+			keys = append(keys, key)
+		}
+	}
+	return keys
+}
+
+// Size by Cache.
+func (c *Cache) Size() int {
+	return len(c.data)
+}
+
+var caches = []*Cache{}
+
+// NewCache new cache.
+func NewCache(expire time.Duration, LRU ...bool) *Cache {
+	cache := &Cache{
+		data:     make(map[interface{}]interface{}),
+		access:   make(map[interface{}]time.Time),
+		Expire:   expire,
+		Callback: func(key, value interface{}) {},
+	}
+	if len(LRU) > 0 {
+		cache.LRU = LRU[0]
+	}
+	if len(caches) == 0 {
+		go func() {
+			ticker := time.NewTicker(1 * time.Second)
+			for now := range ticker.C {
+				for _, c := range caches {
+					c.Clean(now)
+				}
+			}
+		}()
+	}
+	caches = append(caches, cache)
+	return cache
+}
+
+// NewCallbackCache new have del callback cache.
+func NewCallbackCache(expire time.Duration, callback func(key, value interface{}), LRU ...bool) *Cache {
+	cache := NewCache(expire, LRU...)
+	cache.Callback = callback
+	return cache
+}
---
+++ cache/cache_test.go
@@ -0,0 +1,130 @@
+package cache
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCache(t *testing.T) {
+	cache := NewCache(1 * time.Second)
+	cache.Set("key1", "value1")
+	cache.Set("key2", "value2")
+
+	assert.Equal(t, 2, cache.Size(), "Size")
+
+	value, ok := cache.Get("key2")
+	assert.Equal(t, "value2", value, "Get")
+	assert.True(t, ok, "Get ok")
+
+	cache.Set("key2", "value3")
+	value, ok = cache.Get("key2")
+	assert.Equal(t, "value3", value, "Get")
+	assert.True(t, ok, "Get ok")
+
+	cache.Del("key1")
+	assert.Equal(t, 1, cache.Size(), "Size")
+
+	assert.Equal(t, 1, len(cache.Keys()), "Keys")
+
+	time.Sleep(2 * time.Second)
+	assert.Equal(t, 0, cache.Size(), "time")
+}
+
+func BenchmarkCacheSize(b *testing.B) {
+	cache := NewCache(20 * time.Second)
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		cache.Size()
+	}
+}
+
+func BenchmarkCachePut(b *testing.B) {
+	cache := NewCache(20 * time.Second)
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		cache.Set("key1", "value1")
+	}
+}
+
+func BenchmarkMapPut(b *testing.B) {
+	cache := map[string]string{}
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		cache["key1"] = "value1"
+	}
+}
+
+func ExampleCache() {
+	cache := NewCache(3*time.Second, true)
+	cache.Set("key1", "value1")
+	cache.SetByDuration("key2", "value2", time.Second)
+	cache.Set("key3", "value3")
+
+	fmt.Println(cache.Get("key1"))
+	fmt.Println("init size:", cache.Size())
+	time.Sleep(2 * time.Second)
+	fmt.Println(cache.Get("key3")) // reset expire time.
+	fmt.Println("2 second:", cache.Size())
+	time.Sleep(2 * time.Second)
+	fmt.Println("4 second:", cache.Size())
+
+	// Output:
+	// value1 true
+	// init size: 3
+	// value3 true
+	// 2 second: 2
+	// 4 second: 1
+}
+func ExampleNewCallbackCache() {
+	cache := NewCallbackCache(3*time.Second, func(key, value interface{}) {
+		fmt.Println("del:", key, value)
+	}, true)
+	cache.Set("key1", "value1")
+	cache.SetByDuration("key2", "value2", time.Second)
+	cache.Set("key3", "value3")
+
+	fmt.Println(cache.Get("key1"))
+	fmt.Println("init size:", cache.Size())
+	time.Sleep(2 * time.Second)
+	fmt.Println(cache.Get("key3")) // reset expire time.
+	fmt.Println("2 second:", cache.Size())
+	time.Sleep(2 * time.Second)
+	fmt.Println("4 second:", cache.Size())
+
+	// Output:
+	// value1 true
+	// init size: 3
+	// del: key2 value2
+	// value3 true
+	// 2 second: 2
+	// del: key1 value1
+	// 4 second: 1
+}
+
+func TestCachePut1(t *testing.T) {
+	c := NewCache(3 * time.Second)
+	c.Set("3", "3")
+	time.Sleep(4 * time.Second)
+	assert.Equal(t, 0, c.Size(), "exists")
+}
+
+func TestCachePut2(t *testing.T) {
+	c := NewCache(3 * time.Second)
+	time.Sleep(1 * time.Second)
+	c.Set("3", "3")
+	time.Sleep(4 * time.Second)
+	assert.Equal(t, 0, c.Size(), "exists")
+}
+func TestCachePut3(t *testing.T) {
+	c := NewCache(2*time.Second, true)
+	c.Set(1, 1)
+	time.Sleep(1 * time.Second)
+	c.Get(1)
+	time.Sleep(1 * time.Second)
+	c.Get(1)
+	time.Sleep(1 * time.Second)
+	assert.Equal(t, 1, c.Size(), "LRU")
+}
